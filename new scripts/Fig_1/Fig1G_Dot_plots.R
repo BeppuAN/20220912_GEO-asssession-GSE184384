333b514cecddc9733ddffcb20b39f9dfa5df026a
BeppuAN <55115226+BeppuAN@users.noreply.github.com>
2022-08-22 11:57:56 -0700

    Delete new scripts directory

--- new scripts/Fig_1/Fig1G_Dot_plots.R
+++
@@ -1,9 +0,0 @@
-#code used to visualize transcriptional landscape of lung serous cells
-#load in relevant datasets and libraries
-library(Seurat)
-library(ggplot2)
-library(cowplot)
-epitimecourse.integrated.V2 <- readRDS("/Volumes/20190816.singlecellRNAseq/20220112.scRNAseq_Scripts/epitimecourse.integrated.V2.rds")
-x <- epitimecourse.integrated.V2
-
-DotPlot(x, features = c("rna_Msln", "rna_Bpifa1", "rna_Ltf", "rna_Scgb3a2", "rna_Scgb1a1", "rna_Cldn10"), group.by = "anno")
